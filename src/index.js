9cb201022520cf67eee0c10f05ed4bdfce26f5e3
Helder <heldrida@gmail.com>
2016-08-12 17:28:10 +0100

    added lodash and used the debounce fn to delay the search all

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import apiKeys from '../api_keys';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
 
 const API_KEY = apiKeys.youtube_api;
 
@@ -35,9 +36,12 @@ class App extends Component {
 	}
 
 	render() {
+
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar onSearchTermChange={ term => this.videoSearch(term) } />
+				<SearchBar onSearchTermChange={ videoSearch } />
 				<VideoDetail video={ this.state.selectedVideo }/>
 				<VideoList 
 					onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
