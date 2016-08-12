09135957c3cb4d9f2e5a67e1ee3324432cf3bae0
Helder <heldrida@gmail.com>
2016-08-12 16:01:31 +0100

    import the youtube library; placed the data fetching call in the most parent component;

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import apiKeys from '../api_keys';
 import SearchBar from './components/search_bar';
 
 const API_KEY = apiKeys.youtube_api;
+
+YTSearch({ key: API_KEY, term: 'skateboards' }, function (data) {
+	console.log(data);
+});
 
 const App = () => {
 	return (
