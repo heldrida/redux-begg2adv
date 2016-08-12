2cad8040232bab398792704dceb794712d127117
Helder <heldrida@gmail.com>
2016-08-12 13:12:23 +0100

    created place holder search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import apiKeys from '../api_keys';
+import SearchBar from './components/search_bar';
 
 const API_KEY = apiKeys.youtube_api;
 
 const App = () => {
-	return <div>Hi!</div>
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	)
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
