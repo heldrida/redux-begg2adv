55a28a9107635d3e2e6130c188ec9b831a2d0f16
Helder <heldrida@gmail.com>
2016-08-12 16:04:28 +0100

    refactor the entry point main App component from functional to React Component, to allow getting hold of state

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import apiKeys from '../api_keys';
 import SearchBar from './components/search_bar';
 
@@ -11,12 +10,22 @@ YTSearch({ key: API_KEY, term: 'skateboards' }, function (data) {
 	console.log(data);
 });
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	)
+// const App = () => {
+// 	return (
+// 		<div>
+// 			<SearchBar />
+// 		</div>
+// 	)
+// }
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
