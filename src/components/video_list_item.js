c207bc862a45f8e90007127eaf4bd1df5a8e54e8
Helder <heldrida@gmail.com>
2016-08-12 16:33:48 +0100

    created DOM nodes to place the youtube data

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const VideoListItem = () => {
-	return <li>video</li>
+// es6 syntax sugar, pass video property that exists in props
+// without need to declare a new video const with the value of props.video
+const VideoListItem = ({ video }) => {
+	return (
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img className="media-object" />
+				</div>
+			</div>
+			<div className="media-body">
+				<div className="media-heading">
+				</div>
+			</div>
+		</li>
+	);
 }
 
 export default VideoListItem;
